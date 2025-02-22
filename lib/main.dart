9f0a37aabb4bdc69aa79544b41cef7b72b181e03
Shivansh Tiwari <sachintiwari.edu@gmail.com>
2025-02-22 21:25:40 +0530

    quiz app

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/question_screen.dart';
+
+void main() {
+  runApp(const Quiz());
+}
---
+++ lib/quiz_questions.dart
@@ -0,0 +1,6 @@
+class QuizQuestion {
+QuizQuestion
+
+  final String text;
+  final List <String> answers;
+}
